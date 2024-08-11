9bb8f6c4bf887ebdfe69152e3d9134877b29a6ed
STARCK-RADOUA <122606108+STARCK-RADOUA@users.noreply.github.com>
2024-08-11 21:40:52 +0100

    com

--- ExpressChezVous/src/screens/LoadingScreen.js
+++ ExpressChezVous/src/screens/LoadingScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import io from 'socket.io-client';
 import * as Device from 'expo-device';
-const socket = io('http://192.168.8.131:4000'); // Replace with your server's URL
+const socket = io('http://192.168.8.129:4000'); // Replace with your server's URL
 
 const LoadingScreen = ({ navigation }) => {
   const [isActivated, setIsActivated] = useState(false);
